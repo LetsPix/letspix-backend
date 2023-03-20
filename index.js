dbaf9336a941f06a9fd7e05a093fa8bff44a4f7a
Brian <bfeddes@hotmail.com>
2023-03-20 15:08:57 -0500

    Added MongoDB Connection

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-const express = require("express");
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const constants = require('./ConnectionConstants.js');
 const app = express();
+app.use(express.static("public"))
 
-app.use(express.static(__dirname + '/client'))
+const uri = constants.uri;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/', (req, res) => {
+  res.redirect('/home')
+});
+
+app.get('/home',(req, res) => {
+  res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/home/api/data/netflix', async (req, res) => {
+  try {
+    await client.connect();
+    const collection = client.db(constants.databaseName).collection(constants.collectionName);
+    const data = await collection.find({}).toArray();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  } finally {
+    await client.close();
+  }
+});
+
+
 
 const port = process.env.PORT || 3000
-app.get('/test', function(request, response) {
-	response.type('text/plain')
-	response.send('Node.js and Express running on port='+port)
-})
 
 app.listen(port, function() {
-	console.log("Server is running at http://localhost:3000/")
+	console.log(`Server is running at ${port}`)
 })
---
+++ readOne.js
@@ -0,0 +1,20 @@
+const { ObjectId } = require('bson');
+
+const constants = require('./ConnectionConstants.js');
+let MongoClient = require('mongodb').MongoClient;
+
+async function readByID(uri, databaseName, collectionName, id) {
+	const client = new MongoClient(uri);
+	const result = await client.db(databaseName).collection(collectionName).findOne({title: `${id}`});
+    console.log(result);
+	client.close()
+}
+
+// The first user parameter (argv[2]) must be the ObjectID to find.
+if (process.argv[2] === undefined) {
+	console.log('ObjectID Required')
+} else {
+	readByID(constants.uri, constants.databaseName, constants.collectionName, process.argv[2] /* ObjectID */);
+}
+
+
